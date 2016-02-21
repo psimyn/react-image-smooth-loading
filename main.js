89a1181dd7cf7a5c7768e5d99853f60ba1606b70
Gabriel Rubens <grsabreu@gmail.com>
2016-02-21 19:26:45 -0300

    Build gh-pages

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 (function(){
   var Img = React.createFactory(ReactImg.default);
-
-  Img.globalPlaceholder =
-    'http://astrocoders.com/react-image-smooth-loading/images/placeholder.png';
-
+  
   var App = React.createClass({
     render(){
       var imgs = this._getImageList().map(function(url){
         return React.createElement('div', {
-          style: { width: '20%', 'float': 'left', marginLeft: '10px' },
+          style: { width: '20%', float: 'left', 'margin-left': '10px' },
         }, Img({ src: url, key: url}));
       });
 
